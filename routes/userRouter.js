1e71966f7f512c75f65dd24b2a6aeec7b1ab2e00
priyanshusingh75177-wq <priyanshusingh00031@gmail.com>
2025-11-23 02:31:11 +0530

    confirmed my MVC format

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,36 +4,16 @@ const User = require("../models/user.js");
 const wrapasync = require("../utils/wrapasync.js");
 const passport= require("passport");
 const {saveRedirectUrl}=require("../middleware.js")
+const userController = require("../controllers/users.js")
 
-router.get("/signup",(req ,res)=>{
-  res.render("user/signup.ejs")
-})
+router.get("/signup",userController.renderSignupForm)
 
 router.post(
   "/signup",
-  wrapasync(async (req, res) => {
-    try {
-      let { username, email, password } = req.body;
-      const newUser = new User({ email, username });
-      const registeredUser = await User.register(newUser, password);
-      console.log(registeredUser);
-      req.login(registeredUser, (err) => {
-        if (err) {
-          return next(err);
-        }
-        req.flash("success", "Welcome to Wanderlust!");
-        res.redirect("/listings");
-      });
-    } catch (e) {
-      req.flash("error", e.message);
-      res.redirect("/signup");
-    }
-  })
+  wrapasync(userController.signup)
 );
 
-router.get("/login",(req,res)=>{
-    res.render("user/login.ejs")
-})
+router.get("/login",userController.renderLoginForm)
 
 
 router.post(
@@ -43,21 +23,9 @@ router.post(
     failureRedirect: "/login",
     failureFlash: true,
   }),
-  async (req, res) => {
-    req.flash("success", "Welcome to Wanderlust! You are logged in!");
-    let redirectUrl=res.locals.redirectUrl ||"listings";
-    res.redirect(redirectUrl); // You might want to redirect somewhere after login
-  }
+  userController.login
 );
-router.get("/logout", (req, res, next) => {
-    req.logout((err) => {
-        if (err) {
-            return next(err);
-        }
-        req.flash("success", "you are logged out!");
-        res.redirect("/listings");
-    });
-});
+router.get("/logout",userController.logout);
 
 
 
