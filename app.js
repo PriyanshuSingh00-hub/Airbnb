24193c7bd9db85a7fdf929108595cf70ed4e96a9
Priyanshu singh <priyanshusingh00031@gmail.com>
2026-01-09 19:40:21 +0530

    Fixed errors

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const listingsRouter = require("./routes/listing.js");   //listinggggg routerss!
 const reviewRouter = require("./routes/review.js");
 const userRouter= require("./routes/userRouter.js");
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust.";
 const dbUrl=process.env.ATLASDB_URL
 
 // -------------------------
